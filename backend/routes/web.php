f3ad56316bd37232f0ffa788a311b35805138b8d
aowebtech13 <adesokanadedeji10@gmail.com>
2026-02-05 01:51:09 -0800

    complete

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "API is running";
 });
+
+Route::get('/login', function () {
+    return response()->json(['message' => 'Please login'], 401);
+})->name('login');
